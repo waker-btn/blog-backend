5ed9da5d9d84e8efcdb5989694e4432d25655131
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-10-30 14:37:36 +0000

    GET authors updated

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 import pool from "./pool.js";
 
 async function getAuthors() {
-  const { rows } = await pool.query("SELECT * FROM authors");
+  const { rows } = await pool.query("SELECT author_id, name, bio, date_joined FROM authors");
   return rows;
 }
 
