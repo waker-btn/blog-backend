46d285ef04474c1e8a12adf51f3ceb45b8e634ad
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-10-30 15:05:04 +0000

    Add CORS

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config.js";
 import authorRouter from "./routes/authorRouter.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use("/authors", authorRouter);
 app.get("/", (req, res) => {
